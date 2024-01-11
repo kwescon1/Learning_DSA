2d49c022ee9b9003a25fd539aa5c8dacfa8263bc
Odame <kwesi.danquah@daaseng.com>
2024-01-11 00:22:04 +0000

    Python implementation

---
+++ algorithms/binary_search/binary_search.py
@@ -0,0 +1,25 @@
+def binarySaerch(list,item):
+    
+    low = 0
+    high = len(list) - 1
+    
+    while low <= high:
+        midPoint = (low + high) // 2 #Integer division for midpoint
+        
+        guess = list[midPoint]
+        
+        if guess == item: 
+            return midPoint # the position of the item
+        
+        if guess < item : # Item is in upper half of the list
+            # increase the low to the next value after the midpoint value
+            low = midPoint + 1
+        else :
+            # Item is in lower half of the list
+            
+            # decrease high to the value before the midpoint value
+            
+            high = midPoint - 1
+        
+    return None # Item not found
+        
